2730fd93b4546bb193dc39d594db4e928aa4234c
doug.c.sant@hotmail.com <Pro010889>
2019-08-26 18:18:19 -0300

    segundo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,12 @@
 const  express = require('express');
 const routes = require ('./routes');
+const mongoose = require('mongoose');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://admin:Pro010889@cluster0-rkrag.mongodb.net/omnistack?retryWrites=true&w=majority',{
+    useNewUrlParser :true
+});
 server.use(express.json());
 server.use(routes);
 server.listen(3333);
