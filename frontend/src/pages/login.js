ca334c01eed2de3efcc724d2ec6a288b853096f5
doug.c.sant@hotmail.com <Pro010889>
2019-08-29 18:02:39 -0300

    segunda

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './login.css';
 
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.svg'; 
 export default function login(){
     return (
         <img src ={logo} alt="Tindev"/>
